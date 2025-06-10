e3e84c318067af1d40e8ab9b959b17349f8de133
Jayvis Hui <redgamer80808@gmail.com>
2025-06-11 10:32:44 +1200

    commit

--- HSKF.py
+++ HSKF.py
@@ -3,16 +3,34 @@ from tkinter import *
 from tkinter import colorchooser
 import cv2
 
-
-
-
 def show_frame(frame): 
     frame.tkraise()
-
-def shutthefuckup(root): #function to close Hueseeker
-    root.destroy() 
 
  
 def colp(gooningL): #function to open the color gui
     usecol = colorchooser.askcolor()
     recol = Label(gooningL, text=usecol).pack()
+
+
+def opencamera(cap, dihchees, swidth, sheight):
+
+    ret,frame = cap.read()
+    if not ret:
+        print("failed :(")
+        return
+    
+    opencv_image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+
+    resized = cv2.resize(opencv_image, (swidth, sheight))
+
+    captureim = PILImage.fromarray(resized)
+    
+    photoim = ImageTk.PhotoImage(image=captureim)
+
+    dihchees.photoimage = photoim
+
+    dihchees.configure(image=photoim)
+
+    dihchees.after(10,lambda: opencamera(cap, dihchees, swidth, sheight))
+
+    
